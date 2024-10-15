dd60363a9871d0e4fddd490a4b350bb2b3e04a95
hardikdhalla <dhallahardik31@gmail.com>
2024-10-15 20:38:37 +0530

    Add files via upload

---
+++ call_by_reference.cpp
@@ -0,0 +1,24 @@
+//Hardik Dhalla
+//23070123058
+#include <iostream>
+#include<string>
+using namespace std;
+
+void swap(int *x , int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int main() {
+    int a = 5, b = 2;
+    swap(a,b);
+    cout << "Value of the a is:" << a<<endl;
+    cout << "Value of the b is:" << b<<endl;
+    return 0;
+}
+
+/* OUTPUT
+Value of the a is:2
+Value of the b is:5
+*/
